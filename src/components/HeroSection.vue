231227a1d65dcb0d1146d8f58003f3beadbe5251
abiodunnoah <ajaoolasco@gmail.com>
2025-12-29 20:20:57 +0100

    feat: Add resume PDF.

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -13,7 +13,9 @@
 
       <div class="buttons">
         <a href="#projects" class="btn primary">View Projects</a>
-        <a href="/cv.pdf" class="btn secondary" target="_blank">Download CV</a>
+        <a href="/Ajao Abiodun N. - Resume.pdf" class="btn secondary" target="_blank"
+          >Download CV</a
+        >
       </div>
     </div>
   </section>
